73751751ae4ab32dee44597e59935b33df909bd9
Erick140297 <erickmonterrubio67@outlook.com>
2022-11-11 19:22:02 -0600

    Landing Page finished

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { getDiets } from "../../redux/actions";
+import s from "./LandingPage.module.css";
 
 const LandingPage = () => {
   const dispatch = useDispatch();
@@ -11,12 +12,17 @@ const LandingPage = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <h2>Welcom to FoodApp</h2>
-      <p>FoodApp is a single page aplication developed wiht react, redux, expres, sequelize and postgres that use the API of spoonacular.</p>
-      <Link to={"/home/recipes"} style={{textDecoration: 'none'}}>
-        <button>FoodApp</button>
-      </Link>
+    <div className={s.img}>
+      <div className={s.topBar}>
+        <h2>Welcom to FoodApp</h2>
+        <Link to={"/home/recipes"} style={{ textDecoration: "none" }}>
+          <button className={s.btn}>FoodApp</button>
+        </Link>
+      </div>
+      <p className={s.text}>
+        FoodApp is a single page aplication developed wiht react, redux, expres,
+        sequelize and postgres that use the API of spoonacular.
+      </p>
     </div>
   );
 };
